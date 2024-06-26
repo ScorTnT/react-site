be822ee9846e244f29e8edb9e97d30ae8f343f2f
ScorTnT <flalxlemdkdlel@naver.com>
2024-06-26 12:21:02 +0900

    ING login signUp

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import logo from './logo.svg';
 import {useState} from 'react';
 import './App.css';
-//
+
 function HEAD(props){
   return (<article>
     <h2>{props.title}</h2>
     {props.body}
   </article>)
 }
-function Control(props){
-  return (<p><a href={`/`+props.name} onClick={function(event){
-    props.setType(props.name);
+function Link(props){
+  return (<p><a href={`/`+props.value} onClick={function(event){
+    props.setType(props.value);
     event.preventDefault();
   }}>{props.name}</a></p>)
 }
-function Create(props){
+function SignUp(props){
   return(
-    <form onSubmit={function(event){
+    <><form action="/login" method="POST" onSubmit={function(event){
       event.preventDefault();
     }}>
     <p>
       <input type="submit" value="Create"></input>
     </p>
     </form>
+    </>
   )
 }
-function UserMod(props){
-  props.setUser(props.user);
-  return ;
+function LoginP(props){
+  //props.setUser(props.user);
+  return (<form onSubmit={function(event){
+    event.preventDefault();
+  }}>
+  <p>
+    <input type="submit" value="Login"></input>
+  </p>
+  <Link name="SignUp"></Link>
+  </form>);
 }
 function App() {
   const [pageType, setType] = useState('WELCOME');
@@ -37,38 +45,38 @@ function App() {
   var contents='';
   var body = '';
 
-  if(pageType === 'WELCOME'){
-    mainBar = <HEAD title="WELCOME" body="Hello, World"></HEAD>
-    controlBar = <><Control setType={setType} name="Create"></Control>
-    <Control setType={setType} name="Login"></Control></>
+  if(pageType === 'WELCOME' || pageType === 'HOME'){
+    mainBar = <HEAD title={pageType} body="Hello, World"></HEAD>
+    controlBar = <><Link setType={setType} name="Sign Up" value = "SIGNUP"></Link>
+    <Link setType={setType} name="LOGIN"></Link></>
   }
-  else if(pageType === 'Login'){
-    mainBar = <HEAD title="Login"></HEAD>
-    body = <UserMod setUser={setUser} user="guest"></UserMod>
-    body += userName;
+  else if(pageType === 'LOGIN'){
+    if(userName !== 'guest') setUser('guest');
+    mainBar = <HEAD title="LogIn" body="Put your Id & Pass Word"></HEAD>
+    controlBar = <><Link setType={setType} name="HOME" value="HOME"></Link></>
+    contents = <LoginP setUser={setUser} user="guest"></LoginP>
   }
-  else if(pageType === 'Create'){
+  else if(pageType === 'SIGNUP'){
     mainBar = <><HEAD title="Create Page"></HEAD></>
-    controlBar = <><Control setType={setType} name="WELCOME"></Control></>
-    contents = <><Create></Create></>
+    controlBar = <><Link setType={setType} name="HOME" value></Link></>
+    contents = <><SignUp></SignUp></>
   }
-  console.log(userName);
+  console.log(userName,pageType);
+
   return (
-    <html>
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div className="Nav">
-        {controlBar}
-      </div>
-      <div className="MainDiv">
-        {mainBar}
-        {contents}
-        {body}
-      </div>
-      </div>
-    </html>
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+    <div className="Nav">
+      {controlBar}
+    </div>
+    <div className="MainDiv">
+      {mainBar}
+      {contents}
+      {body}
+    </div>
+    </div>
   );
 }
 
